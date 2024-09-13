f50b20eb9e2d3886713de219154f3999df457182
sgwood <sgwood@qq.com>
2024-09-13 18:28:18 +0800

    Update FieldController.java
    update

--- yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/powerbi/FieldController.java
+++ yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/powerbi/FieldController.java
@@ -6,16 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
-
+import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserId;
+import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserNickname;
+import static cn.iocoder.yudao.framework.security.core.util.SecurityFrameworkUtils.getLoginUserDeptId;
 /**
  *  下钻控制器
  */
 @RestController
-@RequestMapping("/field/test")
+@RequestMapping("/star/field/test")
 public class FieldController {
 
     @GetMapping("/get")
     public CommonResult<String> test() {
+        System.out.println("getLoginUserId:"+getLoginUserId());
+        System.out.println("getLoginUserDeptId():"+getLoginUserDeptId());
+        System.out.println("getLoginUserNickname():"+getLoginUserNickname());
         return success("ok34");
     }
 }
