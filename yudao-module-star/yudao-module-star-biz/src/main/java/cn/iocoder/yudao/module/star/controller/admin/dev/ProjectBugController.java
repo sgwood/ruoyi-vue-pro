d2027ab783fe54b238a873cd2b2d5dd3907b2673
sgwood <sgwood@qq.com>
2025-01-14 16:31:56 +0800

    1

--- yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/dev/ProjectBugController.java
+++ yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/dev/ProjectBugController.java
@@ -1,4 +1,26 @@
 package cn.iocoder.yudao.module.star.controller.admin.dev;
 
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
+
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.error;
+import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
+
 public class ProjectBugController {
+
+    public CommonResult<String> getProduct() {
+        return success("hello");
+    }
+
+    /**
+     *  get bugs
+     * @return
+     */
+    public CommonResult<String> getBug() {
+        return success("hello");
+    }
+
+
+    public CommonResult<String> getProductListById() {
+        return error(301,"error");
+    }
 }
