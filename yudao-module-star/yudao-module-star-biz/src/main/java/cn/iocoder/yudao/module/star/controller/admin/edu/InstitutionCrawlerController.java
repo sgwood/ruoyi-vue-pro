404528df7b8a228e825b2697151182a9ac408cb3
sgwood <sgwood@qq.com>
2025-05-14 17:27:24 +0800

    save to db

--- yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/edu/InstitutionCrawlerController.java
+++ yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/edu/InstitutionCrawlerController.java
@@ -1,9 +1,11 @@
 package cn.iocoder.yudao.module.star.controller.admin.edu;
 
+import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.module.star.dal.dataobject.edu.InstitutionListResponse;
 import cn.iocoder.yudao.module.star.service.edu.InstitutionCrawlerService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
         @GetMapping("/institutions")
         public InstitutionListResponse getInstitutions() {
             return institutionCrawlerService.crawlInstitutions();
+        }
+    @PostMapping("/save")
+        public CommonResult<Boolean> save() {
+            institutionCrawlerService.crawlAndSaveInstitutions();
+            return CommonResult.success(true);
         }
     }
 
