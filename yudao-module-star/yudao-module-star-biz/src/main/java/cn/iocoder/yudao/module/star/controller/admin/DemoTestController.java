6b321530d13a2b824e9ea3e9e9eef07bcdd85326
sgwood <sgwood@qq.com>
2025-05-15 18:06:39 +0800

    commited

--- yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/DemoTestController.java
+++ yudao-module-star/yudao-module-star-biz/src/main/java/cn/iocoder/yudao/module/star/controller/admin/DemoTestController.java
@@ -11,15 +11,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
 
+/**
+ * 管理后台中与 Test 相关功能的控制器类，
+ * 提供获取 test 信息和保存 test 信息的接口。
+ */
 @Tag(name = "管理后台 - Test")
 @RestController
 @RequestMapping("/demo/test")
 @Validated
 public class DemoTestController {
+
+    /**
+     * 获取 test 信息的接口。
+     * 该接口处理 HTTP GET 请求，返回包含 test 信息的通用结果。
+     *
+     * @return 封装了 test 信息的通用结果对象，包含操作状态和具体信息
+     */
     @GetMapping("/get")
     @Operation(summary = "获取 test 信息")
     public CommonResult<String> get() {
+        // 打印调试信息
+    /**
+     * 保存 test 信息的接口。
+     * 该接口处理 HTTP POST 请求，返回保存操作的通用结果。
+     *
+     * @return 封装了保存操作结果的通用结果对象，包含操作状态和结果信息
+     */
+        // 返回包含具体 test 信息的成功结果
+        // 注释掉的返回语句，可用于返回不同测试结果
+        // 打印调试信息
         System.out.println("say hello3245");
+System.out.println("aaaa");
+
         // return success("false");
         return success("say hello21333 abc ");
     }
@@ -30,3 +53,4 @@ public class DemoTestController {
         return success("sss");
     }
 }
+
